d4441d45937948ee1d687581db62cb5d642bef5a
hemant0605 <60573871+hemant0605@users.noreply.github.com>
2025-08-31 08:07:55 +0000

    added script

---
+++ script.js
@@ -0,0 +1,14 @@
+function sendToWhatsApp(e) {
+  e.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let message = document.getElementById("message").value;
+
+  let whatsappUrl = "https://wa.me/918825114380?text=" 
+      + "Hello, my name is " + name + 
+      ". My phone is " + phone + 
+      ". Message: " + message;
+
+  window.location.href = "thankyou.html"; // redirect to thank you page
+  window.open(whatsappUrl, "_blank"); // open WhatsApp chat
+}
